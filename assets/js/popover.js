8c53b97749dd1e3b0a0bdd67c3cd6f6ddb01f564
Zefram Lou <zeframlou@gmail.com>
2020-10-12 23:07:01 -0700

    Auto-generated commit

---
+++ assets/js/popover.js
@@ -0,0 +1,30 @@
+//
+// popover.js
+// Theme module
+//
+
+'use strict';
+
+(function () {
+  //
+  // Variables
+  //
+
+  var toggle = document.querySelectorAll('[data-toggle="popover"]')
+
+  //
+  // Functions
+  //
+
+  function init (toggle) {
+    $(toggle).popover()
+  }
+
+  //
+  // Events
+  //
+
+  if (toggle) {
+    init(toggle)
+  }
+})()
